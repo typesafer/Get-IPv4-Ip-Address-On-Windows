d3c49a20f090f29ef97407b0d3764796d1d71ce9
Geoffrey <thismuller@gmail.com>
2014-12-30 18:09:34 +0300

    Create ipv4_windows.php

---
+++ ipv4_windows.php
@@ -0,0 +1,13 @@
+function getLocalIP(){
+    exec("ipconfig /all", $output);
+        foreach($output as $line){
+            if (preg_match("/(.*)IPv4 Address(.*)/", $line)){
+                $ip = $line;
+                $ip = str_replace("IPv4 Address. . . . . . . . . . . :","",$ip);
+                $ip = str_replace("(Preferred)","",$ip);
+            }
+        }
+    return $ip;
+}
+
+echo $ip = getLocalIP();
